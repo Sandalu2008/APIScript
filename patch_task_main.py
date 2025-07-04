b967e3ff48c3c1964a0bf2a37fce7e4cb88398db
Pradeep Suranga <bapsuranga@gmail.com>
2025-07-04 03:27:55 -0700

    commit 1

---
+++ patch_task_main.py
@@ -0,0 +1,55 @@
+import requests
+import json
+import time
+
+BASE_URL = "http://localhost:8000/tasks"
+
+def partial_update_task(task_id):
+    url = f"{BASE_URL}/{task_id}"
+    # Only send the fileds you want to change
+    patch_data = {
+        "status" : "completed"
+    }
+    try: 
+        response = requests.patch(url, json=patch_data)
+        response.raise_for_status() # Raise an exception for HTTP
+        updated_task = response.json()
+        print(f"Task {task_id} partically updated successfully:")
+        print(json.dumps(updated_task, indent=2))
+
+    except requests.exceptions.HTTPError as e:
+        print(f"Error updating task: {e}")
+        if e.response.status_code == 400:
+            print(f"Bad Request {e.response.json().get('error', 'unknown')}' not found.")
+    except requests.exceptions.RequestException as e:
+        print(f"Error connecting to API: {e}")
+
+def create_new_task():
+    """ Creates a new taks by sending a POST request to the API"""
+    print("----Createing a new task----")
+    new_task_data = {
+        "title": "Plan weekend trip2",
+        "description": "Reserach, destination, book flights, arrange",
+        "status": "pending",
+        "dueDate": "2025-07-15"
+        }
+    try: 
+        response = requests.post(BASE_URL, json=new_task_data)
+        response.raise_for_status() # Raise an exception for HTTP
+        created_task = response.json()
+        print("Task created successfully:")
+        print(json.dumps(created_task, indent=2))
+
+
+    except requests.exceptions.HTTPError as e:
+        print(f"Error creating task: {e}")
+        if e.response.status_code == 400:
+            print(f"Bad Request {e.response.json().get('error', 'unknown')}' not found.")
+    except requests.exceptions.RequestException as e:
+        print(f"Drror connecting to API: {e}")
+    print("-" * 30)
+
+if __name__ == "__main__":
+    #ensure 'task456' exists or  run 'get_tasks.py' and 'post_tsk
+    partial_update_task("task456")
+    partial_update_task("nonexistent_task_for_patch") # Will result
